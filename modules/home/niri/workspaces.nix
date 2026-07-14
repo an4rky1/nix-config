fb0fc2381ce6fd5e14660c7cad8bd6d5fcf9d3c7
anarky <anarky13@proton.me>
2026-07-14 16:03:30 +0300

    fix: remove unsupported icon option from niri workspaces

--- modules/home/niri/workspaces.nix
+++ modules/home/niri/workspaces.nix
@@ -3,43 +3,33 @@
   programs.niri.settings.workspaces = {
     "1" = {
       name = "Terminal";
-      icon = "terminal";
     };
     "2" = {
       name = "Browser";
-      icon = "globe";
     };
     "3" = {
       name = "Editor";
-      icon = "code";
     };
     "4" = {
       name = "Chat";
-      icon = "message";
     };
     "5" = {
       name = "Music";
-      icon = "music";
     };
     "6" = {
       name = "Files";
-      icon = "folder";
     };
     "7" = {
       name = "Notes";
-      icon = "notes";
     };
     "8" = {
       name = "Terminal 2";
-      icon = "terminal";
     };
     "9" = {
       name = "Browser 2";
-      icon = "globe";
     };
     "10" = {
       name = "Scratch";
-      icon = "scratchpad";
     };
   };
 }
