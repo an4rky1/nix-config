d1fee238d318c673e6ff8746016432e63d1fa609
anarky <anarky13@proton.me>
2026-07-14 14:37:17 +0300

    fix: correct nvim module import path

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -21,7 +21,7 @@
     ./micro.nix # nano replacement
     ./nemo.nix # file manager
     ./noctalia # desktop environment
-    ./nvim # neovim editor
+    ./nvim/nvim.nix # neovim editor
     ./obsidian.nix
     ./p10k/p10k.nix
     ./packages # other packages
